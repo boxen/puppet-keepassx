d9bffea7e7fa5e5c28d5a7c18147cc83df6a3b44
Yasir Khalid <yasir.khalid@gmail.com>
2015-03-29 13:55:46 +0800

    Introduce parameter whether use KeePassX2 or not, default is no.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,17 @@
 # Examples
 #
 #   include keepassx
-class keepassx {
-  package { 'KeePassX':
-    provider => 'appdmg',
-    source   => 'http://downloads.sourceforge.net/keepassx/KeePassX-0.4.3.dmg'
+class keepassx ( $use_Keepassx_2 = false ) {
+  if $use_Keepassx_2 {
+    package { 'KeePassX 2.0':
+      provider => 'appdmg',
+      source   => 'https://www.keepassx.org/dev/attachments/download/72/KeePassX-2.0-alpha6.dmg'
+    }
+  } else {
+    package { 'KeePassX':
+      provider => 'appdmg',
+      source   => 'http://downloads.sourceforge.net/keepassx/KeePassX-0.4.3.dmg'
+    }
   }
+  
 }
